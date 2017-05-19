ff369e740bc7a60b8ffb97f8b62f4acd869f6854
Julien Vincent <julienlucvincent@gmail.com>
2017-05-19 16:28:35 +0200

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// @flow
+import Head from "next/head"
+
+export default () => (
+	<div>
+		<Head>
+			<title>My Anime List</title>
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+		</Head>
+	</div>
+)
